acb47717ff6c2228e465a6768ede2800203251ce
sysion <sysionng@gmail.com>
2021-03-12 16:45:56 +0100

    ReverseTheNumber.java started

---
+++ ReverseTheNumber.java
@@ -0,0 +1,58 @@
+/**
+* https://www.codechef.com/LRNDSA01/problems/FLOW007
+*
+*
+* Reverse The Number Problem Code: FLOW007
+* Given an Integer N, write a program to reverse it.
+* 
+* Input
+* The first line contains an integer T, total number of testcases. Then follow 
+* T lines, each line contains an integer N.
+* 
+* Output
+* For each test case, display the reverse of the given number N, in a new line.
+*
+*
+* Examples
+* Input
+* 4
+* 12345
+* 31203
+* 2123
+* 2300
+*
+* Output
+* 4
+* 54321
+* 30213
+* 3212
+* 32
+*
+* Time Limit:	1 secs
+* Source Limit:	50000 Bytes
+*/
+
+import java.util.Scanner;
+import java.io.OutputStream;
+import java.lang.Math;
+
+public class ReverseTheNumber {
+	public static void main(String[] args){
+		//int inpNumber = 4;
+		//int inpNumber = 12345;
+		//int inpNumber = 31203;
+		//int inpNumber = 2123;
+		//int inpNumber = 2300;
+		int inpNumber = (int)(23 * Math.pow(10, 7));
+		System.out.println(inpNumber);
+
+		ReverseTheGivenNumber(inpNumber);
+	}
+
+	public static int ReverseTheGivenNumber(int num){
+		
+	}
+
+
+
+}

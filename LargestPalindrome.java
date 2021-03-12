e6b4c47d7203f7d8fc849c027e403f9ed89f973d
sysion <sysionng@gmail.com>
2021-03-12 15:01:16 +0100

    LargestPalindrome.java completed

--- LargestPalindrome.java
+++ LargestPalindrome.java
@@ -26,19 +26,23 @@ public class LargestPalindrome{
 	
 
 	public static void main(String[] args){
-		LargestPalindromeProduct(2);
+		LargestPalindromeProduct(8);
 	}
 
 	public static int LargestPalindromeProduct(int n){
-		int largestPalindromeProduct = 0;
+		long largestPalindromeProduct = 0;
 		boolean found = false;
-		int firstMaxNum = (int)Math.pow(10,(double)n) - 1;
-		int secondMaxNum = (int)Math.pow(10,(double)n) - 1;
+
+		/*firstMaxNum and secondMaxNum should be declared as long to accomodate large 
+		  numbers. From n>=5, numProduct = firstMaxNum * secondMaxNum was returning
+		  wrong result when firstMaxNum and secondMaxNum are declared as int*/ 
+		long firstMaxNum = (long)Math.pow(10,(double)n) - 1;
+		long secondMaxNum = (long)Math.pow(10,(double)n) - 1;
 
 		//System.out.println("firstMaxNum = "+firstMaxNum+", secondMaxNum = "+secondMaxNum);
 
 		while (! found && secondMaxNum != 0) {
-			int numProduct = firstMaxNum * secondMaxNum;
+			long numProduct = firstMaxNum * secondMaxNum;
 			int lenNumProduct = (int) Math.log10(numProduct) + 1;
 
 			//System.out.println("numProduct = "+numProduct);
@@ -46,11 +50,13 @@ public class LargestPalindrome{
 
 			int[] NumProductArray = new int[lenNumProduct];
 
-			int j = lenNumProduct - 1, savedNumber = numProduct;
+			int j = lenNumProduct - 1;
+			long savedNumber = numProduct;
+
 			while (j >= 0){	//break number into array
-				int tmpNumber = savedNumber / (int)Math.pow(10, j);
-				NumProductArray[j] = tmpNumber;
-				savedNumber = savedNumber - (tmpNumber * (int)Math.pow(10, j));
+				long tmpNumber = (savedNumber / (long)Math.pow(10, j));
+				NumProductArray[j] = (int)tmpNumber;
+				savedNumber = savedNumber - (tmpNumber * (long)Math.pow(10, j));
 				j--;
 			}
 
@@ -85,7 +91,7 @@ public class LargestPalindrome{
 		
 		//System.out.println("RESULT = "+largestPalindromeProduct % 1337);
 
-		return largestPalindromeProduct % 1337;
+		return (int)(largestPalindromeProduct % 1337);
 	}
 
 }
